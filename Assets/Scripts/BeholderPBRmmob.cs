7cb37c5b08fa4ad15c0db862ba74b5a5071339ad
KarmaVi <karma9513@gmail.com>
2021-03-26 23:49:36 +0100

    update files

--- Assets/Scripts/BeholderPBRmmob.cs
+++ Assets/Scripts/BeholderPBRmmob.cs
@@ -1,32 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using UnityEngine;
 
-public class BeholderPBRmmob : MonoBehaviour
-{
+//public class BeholderPBRmmob : MonoBehaviour
+//{
+//    GameObject player;
 
-    GameObject player;
-
-
-    private void Start()
-    {
-        player = GameObject.FindWithTag("Player");
-    }
-
-    private void Update()
-    {
-        float direction = player.transform.position.x - transform.position.x;
-        Vector3 pos = transform.position;
-        if (Mathf.Abs(direction) < 2)
-        {
-            pos.x += Mathf.Sign(direction) * Time.deltaTime;
+//    private void Awake()
+//    {
+//        player = GameObject.FindWithTag("Player");
+//    }
+//    private void Update()
+//    {
+//        float direction = player.transform.position.x - transform.position.x;
+//        Vector3 pos = transform.position;
+//        if (Mathf.Abs(direction) < 2)
+//        {
+//            pos.x += Mathf.Sign(direction) * Time.deltaTime;
             
-        }
-        else
-        {
-            pos = new Vector3(Mathf.PingPong(Time.time, 4.0f), 0, 0);
-        }
-        transform.position = pos;
+//        }
+//        //else
+//        //{
+//        //    pos = new Vector3(Mathf.PingPong(Time.time, 4.0f), 0, 0);
+//        //}
+//        transform.position = pos;
 
-    }
-}
+//    }
+//}
